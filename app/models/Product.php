b3c471843c21eac3da3304efbefaf47b13970631
David Torrez <ltorrezs2008@gmail.com>
2015-07-14 12:33:07 -0400

    modelos del cliente y productos implementados

---
+++ app/models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+class Product extends EntityModel
+{
+	public static function findProductByKey($key)
+	{
+		return Product::scope()->where('product_key','=',$key)->first();
+	}
+	public function getName()
+	{
+		return $this->getDisplayName();
+	}
+
+	public function getDisplayName()
+	{
+		if ($this->notes) 
+		{
+			return $this->notes;
+		}
+	}
+
+	public function getProductKey()
+	{
+		if ($this->product_key) 
+		{
+			return $this->product_key;
+		}
+	}
+
+	public function getProductCost()
+	{
+		if ($this->cost) 
+		{
+			return $this->cost;
+		}
+	}
+
+	public function category()
+	{
+		return $this->belongsTo('Category');
+	}
+}
