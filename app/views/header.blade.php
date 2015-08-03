693a52a08f71d50e5a74921874b4b863e95f640a
aleguisf <aguisbert@ipxserver.com>
2015-08-03 17:22:46 -0400

    change alerts

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -126,10 +126,6 @@
     <div class="alert alert-success">{{ Session::get('message') }}</div>
   @endif
 
-  @if (Session::has('warning'))
-  <div class="alert alert-warning">{{ Session::get('warning') }}</div>
-  @endif
-
   @if (Session::has('error'))
   <div class="alert alert-danger">{{ Session::get('error') }}</div>
   @endif
