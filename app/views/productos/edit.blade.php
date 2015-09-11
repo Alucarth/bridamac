0db0b01e96c1905df120f28b47fd507d1ed25704
David Torrez <ltorrezs2008@gmail.com>
2015-09-11 19:56:46 -0400

    flujo productos terminados

--- app/views/productos/edit.blade.php
+++ app/views/productos/edit.blade.php
@@ -1,62 +1,117 @@
 @extends('header')
+@section('title')Nuevo Producto @stop
+  @section('head') @stop
+@section('encabezado') PRODUCTO @stop
+@section('encabezado_descripcion') Editar Producto XD @stop 
+@section('nivel') <li><a href="{{URL::to('productos')}}"><i class="fa fa-cube"></i> Productos y Servicios</a></li><li><i class="glyphicon glyphicon-compressed"></i> Productos</li>
+            <li class="active"> Nuevo </li> @stop
 
 @section('content')
 
 {{Former::framework('TwitterBootstrap3')}}
 
 <div class="panel panel-default">
-	<div class="panel-heading">
-		<div class="row">
-			<div class="col-md-6">
-				@if ($product)
-					<h4>Editar Producto</h4>
-				@else
-					<h4>Nuevo Producto</h4>
-				@endif
-			</div>
-		</div>
-	</div>
-
 	<div class="panel-body">
-
-		{{ Former::open($url)->addClass('col-md-12 warn-on-exit')->method($method)->rules(array(
-	  		
-	  		'product_key' => 'required|match:/[a-zA-Z0-9.-]+/', 
-	  		'notes' => 'required', 
-	  		'cost' => 'required|Numeric', 
-	  		
-	  	)); }}
-
-		@if ($product)
-	    	{{ Former::populate($product) }}
-		@endif
-
+		
+		{{ Former::open($url)->addClass('col-md-12 warn-on-exit')->method($method)}}
+	  	<input name="is_product" type="hidden" value="1">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-4">
+				<legend>Datos de Producto</legend>
+				<div class="row">
+					<div class="col-md-10">
+					{{-- {{ Former::legend('datos de Producto') }} --}}
+						<p >
+							<label>Código*</label>
+							<input type="text" name="product_key" class="form-control" placeholder="Código del Producto" aria-describedby="sizing-addon2" title="Ingrese Código del Producto" pattern="^[a-zA-Z0-9-].{1,}" required disabled  value="{{$product->product_key}}">
+						</p>
+					</div>
+				</div>
 
-				{{ Former::legend('datos de Producto') }}
+		      	{{-- {{ Former::text('product_key')->label('Código')->title('Solo se acepta Letras, Números y guión(-).') }} --}}
+		      	<p>
+			      	<label>Nombre *</label><br>
+			      	<textarea name="notes" placeholder="Nombre del producto" cols="46" rows="3" >{{$product->notes}}</textarea>
+		     	 </p>
+		      	{{-- {{ Former::textarea('notes')->label('Nombre') }} --}}
+		      {{-- 	{{ Former::select('Unidad')->options('', '')->fromQuery(Unidad::all(), 'nombre', 'id')
+															  ->help('Pick some dude')
+															  ->state('warning')}} --}}
+				<p>
+					<label>Unidad</label>
+				
+				 	<select class="form-control" name="unidad_id" >
+						  	@foreach(Unidad::all() as $u)
+						    <option  <?php if($u->id==$product->unidad_id){?>
+						    		SELECTED<?php }?>    value="{{$u->id}}"  >{{$u->nombre}}</option>
+						    
+							@endforeach
+							
+					 </select>	
 
-		      	{{ Former::text('product_key')->label('Código')->title('Solo se acepta Letras, Números y guión(-).') }}
-		      	{{ Former::textarea('notes')->label('Nombre') }}
 
-		      	{{ Former::text('cost')->label('Precio')->title('Solo se acepta números. Ejem: 500.00') }}
+
+					{{-- Former::select('unidad_id')->addOption('','')->label('')
+			                    ->fromQuery(Unidad::all(), 'nombre', 'id')
+			                    ->help('Unidad de medida que manejara el producto')
+					                     --}}
+
+				</p>
+				<div class="row">
+					<div class="col-md-10">
+						<label>Precio *</label>
+					    <input class="form-control" type="text" name="cost" placeholder="Precio del Producto" aria-describedby="sizing-addon2" required title="Solo se acepta números. Ejem: 500.00" pattern=".[0-9.].{1,}" required value="{{$product->cost}}">
+				      	{{-- {{ Former::text('cost')->label('')->title('Solo se acepta números. Ejem: 500.00') }} --}}
+					</div>
+				</div>
+				
+			    
 
 			</div>
-			<div class="col-md-6">
-
-				{{ Former::legend('Categoria') }}
-		    	{{ Former::select('category_id')->label(' ')->fromQuery($categories, 'name', 'id') }}
+			{{-- <div class="col-md-1"></div> --}}
+			<div class="col-md-5">
+				<legend>Categoría</legend>
+				{{-- {{ Former::legend('Categoria') }} --}}
+				<div class="row">
+					
+					<div class="col-md-9">
+						 <select class="form-control" name="category_id" >
+						  	@foreach($categories as $categoria)
+						    <option  <?php if($product->category_id==$categoria->id){?>
+						    		SELECTED<?php }?>  value="{{$categoria->id}}"  >{{$categoria->name}}</option>
+						    
+							@endforeach
+							
+						  </select>	
+					</div>
+					<div class="col-md-3">
+						<a href="{{ url('categorias')}} " class="btn btn-primary" > Categorías </a>	
+					</div>
+				</div>	
+				 
+		    	{{-- {{ Former::select('category_id')->label(' ')->fromQuery($categories, 'name', 'id') }} --}}
 
 			</div>
 		</div>
-		
-		<center class="buttons">
+		<br><br>
+		<div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-2">
+                 <a href="{{ url('productos/') }}" class="btn btn-default btn-sm btn-block">Cancelar</a>
+            </div>
+            {{-- <div class="col-md-1"></div> --}}
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-success dropdown-toggle btn-sm btn-block"> Guardar</button>
+            </div>
+        </div>
 
-		<a href="{{ url('productos/' . ($product ? $product->public_id : '')) }}" class="btn btn-default"> Cancelar </a>
-		<button type="submit" class="btn btn-success dropdown-toggle"> Guardar </button>
 
-		</center>
-
-	{{ Former::close() }}
+		{{ Former::close() }}
+		Nota: (*) Campos requeridos
+	</div>
 </div>
+
+
 @stop
+
+
