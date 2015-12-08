d6a0fd70a0a0751a02b31dc3ba175a7fbd246c38
roy <roiivan324@gmail.com>
2015-12-08 09:17:55 -0400

    ningun cambio

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'bridamac9',
+			'database'  => 'bridamac3',
 			'username'  => 'root',
 			'password'  => 'bandagriss',
 			'charset'   => 'utf8',
--- app/routes.php
+++ app/routes.php
@@ -50,7 +50,7 @@
 
 // facturacion.ipx
 
-Route::group(array('domain' => '{account}.facturacion.ipx'), function()
+Route::group(array('domain' => '{account}.localhost'), function()
 {
 
   /*Llamadas al controlador Auth*/
