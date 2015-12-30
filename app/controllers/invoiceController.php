d0d2d65fdca6a2c68e01c5e171a3a89d7d1fe703
Vrian7ipx <bbarrera@ipxserver.com>
2015-12-29 19:35:36 -0500

    migracion golden

--- app/controllers/invoiceController.php
+++ app/controllers/invoiceController.php
@@ -173,10 +173,51 @@ class InvoiceController extends \BaseController {
 	}
 	public function sql(){
 		//$users = DB::connection('mysql2')->table('users')->get();
-		return "Updated dont try to do it again";
-		$branch_id_golden= 14;
-		$account_id_golden = 10;
-		$user_id_golden = 12;
+                     /// ADDING FUCKING CONTACTS
+            //return "dont try to update this";
+            $branch_id_golden= 2;
+            $account_id_golden = 2;
+            $user_id_golden = 2;
+            //return "brian";
+            $clients=DB::connection('mysql2')->table('clients')->where('account_id',4)->get();
+            foreach ($clients as $client)
+            {
+                $contacts = DB::connection('mysql2')->table('contacts')->where('account_id',4)->where('client_id',$client->id)->get();
+
+                foreach ($contacts as $contact)
+                {
+
+                    $cont = Contact::createNew();
+
+                    $cont->account_id = $account_id_golden;
+                    //$cont->branch_id = $branch_id_golden;
+                    //$cont->user_id = $user_id_golden;
+
+                    $cli = Client::where('account_id',$account_id_golden)->where('public_id',$client->public_id)->first();
+
+                    $cont->client_id = $cli->id;
+                    //return "jae3".$cli->id;
+                    $cont->created_at = $contact->created_at;
+                    $cont->updated_at = $contact->updated_at;
+                    $cont->deleted_at = $contact->deleted_at;
+                    $cont->first_name=$contact->first_name;
+                    $cont->last_name = $contact->last_name;
+                    $cont->email = $contact->email;
+                    $cont->phone = $contact->phone;
+                    $cont->is_primary = $contact->is_primary;
+                    $cont->send_invoice = $contact->send_invoice;
+                    $cont->save();
+                    //print_r($contact);echo "<br><br>";
+                    //echo "-".$cont->first_name."<BR>";                              
+                }
+                //return 0;
+            }
+            echo "contactos agregados con exito";
+            
+		//return "Updated dont try to do it again";
+		$branch_id_golden= 2;
+		$account_id_golden = 2;
+		$user_id_golden = 2;
 		$client_id_ini = 38;
 		$client_id_step = 37;
 		$invoice_id_golden=596;
@@ -189,6 +230,80 @@ class InvoiceController extends \BaseController {
 		// }
 		$product_id_ini = 27;
 		$product_id_step = 26;
+
+
+		/* THIS SCRIPT ITS TO ADD LOGO AND DESIGN*/
+//		 $type_document =TypeDocument::where('account_id',10)->firstOrFail();
+//		 $invoices=Invoice::where("account_id",10)->get();
+//		 foreach ($invoices as $key => $invoice) {
+//		 	$invoice->logo=$type_document->logo;
+//		 	$invoice->javascript=$type_document->javascript_web;
+//		 	$invoice->save();
+//		 }
+
+	//	foreach ($products as $key => $product) {
+	//		$idd=$product->public_id+$product_id_step;
+		//	echo $idd." - ".$product->notes." <br>";
+		//}
+	 	$invoices = DB::connection('mysql2')->table('invoices')->where('account_id',4)->get();
+	 	foreach ($invoices as $key => $invoice) {
+
+	 	$factura = Invoice::createNew();
+	 	$factura->account_id = $account_id_golden;
+	 	$factura->branch_id = $branch_id_golden;
+	 	$client=DB::connection('mysql2')->table('clients')->where('id',$invoice->client_id)->first();
+	 	$factura->client_id = $client->public_id;//+$client_id_step;
+	 	$factura->user_id = $user_id_golden;
+	 	$factura->invoice_status_id = $invoice->invoice_status_id;
+	 	$factura->created_at = $invoice->created_at;
+	 	$factura->updated_at = $invoice->updated_at;
+	 	$factura->deleted_at = $invoice->deleted_at;
+	 	$factura->invoice_number = $invoice->invoice_number;
+	 	$factura->invoice_date = $invoice->invoice_date;
+	 	$factura->due_date = $invoice->due_date;
+	 	$factura->discount = $invoice->discount;
+	 	$factura->po_number = $invoice->po_number;
+	 	$factura->terms = $invoice->terms;
+	 	$factura->public_notes = $invoice->public_notes;
+	 	//$factura->note = $invoice->acc;
+	 	$factura->account_name = $invoice->account_name;
+	 	$factura->account_nit = $invoice->account_nit;
+	 	//$factura->account_uniper = $invoice->;
+	 	$factura->sfc = "SFC-1";
+	 	$factura->branch_name =$invoice->branch_name;
+	 	$factura->address1 = $invoice->address1;
+	 	$factura->address2 = $invoice->address2;
+	 	$factura->phone = $invoice->phone;
+	 	$factura->city = $invoice->city;
+	 	$factura->state =$invoice->state;
+	 	$factura->number_autho = $invoice->number_autho;
+	 	$factura->deadline = $invoice->deadline;
+	 	$factura->key_dosage = $invoice->key_dosage;
+	 	//$factura->type_third = $invoice->;
+	 	$factura->client_nit = $invoice->client_nit;
+	 	$factura->client_name = $invoice->client_name;
+	 	$factura->economic_activity = $invoice->activity_pri;
+	 	$factura->law = $invoice->law;
+	 	$factura->control_code = $invoice->control_code;
+	 	//$factura->qr = $invoice->;
+	 	$factura->debito_fiscal = $invoice->fiscal;
+	 	$factura->importe_neto = $invoice->amount;
+	 	$factura->importe_total = $invoice->subtotal;
+	 	$factura->importe_ice = $invoice->ice;
+	 	//$factura->importe_exento = $invoice->;
+	 	$factura->descuento_total =$invoice->discount;
+	 	$factura->balance = $invoice->balance;
+	 	$factura->logo = 1;
+	 	$factura->javascript = 3;
+	 	//$factura->public_id = $invoice->;
+	 	$factura->save();
+	// 	print_r($factura);
+	 	echo $factura->account_id." ".$factura->branch_id."<br><br>";
+	 }
+
+
+echo "facturas agregadas<br><br><br><br><br>";
+
 		$products=DB::connection('mysql2')->table('products')->where('account_id',4)->get();
 
 
@@ -197,9 +312,9 @@ class InvoiceController extends \BaseController {
 			$it = InvoiceItem::createNew();
 			$it->account_id = $account_id_golden;
 			$invoice=DB::connection('mysql2')->table('invoices')->where('id',$item->invoice_id)->first();
-			$it->invoice_id = $invoice->public_id+$invoice_id_step;
+			$it->invoice_id = $invoice->public_id;//+$invoice_id_step;
 			$product=DB::connection('mysql2')->table('products')->where('id',$item->product_id)->first();
-			$it->product_id = $product->public_id+$product_id_step;
+			$it->product_id = $product->public_id;//+$product_id_step;
 			$it->created_at = $item->created_at;
 			$it->updated_at = $item->updated_at;
 			$it->deleted_at = $item->deleted_at;
@@ -213,80 +328,6 @@ class InvoiceController extends \BaseController {
 			//print_r($it);
 			echo $item->notes."<br><br>";
 		}
-
-		/* THIS SCRIPT ITS TO ADD LOGO AND DESIGN*/
-		// $type_document =TypeDocument::where('account_id',10)->firstOrFail();
-		// $invoices=Invoice::where("account_id",10)->get();
-		// foreach ($invoices as $key => $invoice) {
-		// 	$invoice->logo=$type_document->logo;
-		// 	$invoice->javascript=$type_document->javascript_web;
-		// 	$invoice->save();
-		// }
-
-	//	foreach ($products as $key => $product) {
-	//		$idd=$product->public_id+$product_id_step;
-		//	echo $idd." - ".$product->notes." <br>";
-		//}
-	// 	$invoices = DB::connection('mysql2')->table('invoices')->where('account_id',4)->get();
-	// 	foreach ($invoices as $key => $invoice) {
-
-	// 	$factura = Invoice::createNew();
-	// 	$factura->account_id = $account_id_golden;
-	// 	$factura->branch_id = $branch_id_golden;
-	// 	$client=DB::connection('mysql2')->table('clients')->where('id',$invoice->client_id)->first();
-	// 	$factura->client_id = $client->public_id+$client_id_step;
-	// 	$factura->user_id = $user_id_golden;
-	// 	$factura->invoice_status_id = $invoice->invoice_status_id;
-	// 	$factura->created_at = $invoice->created_at;
-	// 	$factura->updated_at = $invoice->updated_at;
-	// 	$factura->deleted_at = $invoice->deleted_at;
-	// 	$factura->invoice_number = $invoice->invoice_number;
-	// 	$factura->invoice_date = $invoice->invoice_date;
-	// 	$factura->due_date = $invoice->due_date;
-	// 	$factura->discount = $invoice->discount;
-	// 	$factura->po_number = $invoice->po_number;
-	// 	$factura->terms = $invoice->terms;
-	// 	$factura->public_notes = $invoice->public_notes;
-	// 	//$factura->note = $invoice->acc;
-	// 	$factura->account_name = $invoice->account_name;
-	// 	$factura->account_nit = $invoice->account_nit;
-	// 	//$factura->account_uniper = $invoice->;
-	// 	$factura->sfc = "SFC-1";
-	// 	$factura->branch_name =$invoice->branch_name;
-	// 	$factura->address1 = $invoice->address1;
-	// 	$factura->address2 = $invoice->address2;
-	// 	$factura->phone = $invoice->phone;
-	// 	$factura->city = $invoice->city;
-	// 	$factura->state =$invoice->state;
-	// 	$factura->number_autho = $invoice->number_autho;
-	// 	$factura->deadline = $invoice->deadline;
-	// 	$factura->key_dosage = $invoice->key_dosage;
-	// 	//$factura->type_third = $invoice->;
-	// 	$factura->client_nit = $invoice->client_nit;
-	// 	$factura->client_name = $invoice->client_name;
-	// 	$factura->economic_activity = $invoice->activity_pri;
-	// 	$factura->law = $invoice->law;
-	// 	$factura->control_code = $invoice->control_code;
-	// 	//$factura->qr = $invoice->;
-	// 	$factura->debito_fiscal = $invoice->fiscal;
-	// 	$factura->importe_neto = $invoice->amount;
-	// 	$factura->importe_total = $invoice->subtotal;
-	// 	$factura->importe_ice = $invoice->ice;
-	// 	//$factura->importe_exento = $invoice->;
-	// 	$factura->descuento_total =$invoice->discount;
-	// 	$factura->balance = $invoice->balance;
-	// 	//$factura->logo = $invoice->;
-	// 	//$factura->javascript = $invoice->;
-	// 	//$factura->public_id = $invoice->;
-	// 	$factura->save();
-	// 	print_r($factura);
-	// 	echo $factura->account_id." ".$factura->branch_id."<br><br>";
-	// }
-
-
-
-
-
 		//$invoices=DB::connection('mysql2')->table('invoices')->where('account_id',4)->get();
 		//foreach ($invoices as $key => $invoice) {
 		//	echo $invoice->invoice_number."<br>";
